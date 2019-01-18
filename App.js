828b0ca596294fb416266ff1d1628d514fffd7ee
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-01-18 18:42:31 +0530

    Add routing

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { createMaterialTopTabNavigator, createAppContainer, createStackNavigator } from 'react-navigation'
+import StatusBar from './src/components/StatusBar'
+import DeckDetails from './src/components/DeckDetails'
+import DeckList from './src/components/DeckList'
+import CreateDeck from './src/components/CreateDeck'
+
+const Tabs = createMaterialTopTabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Deck List',
+    }
+  },
+  CreateDeck: {
+    screen: CreateDeck,
+    navigationOptions: {
+      tabBarLabel: 'Create Deck',
+    }
+  },
+}, {
+    navigationOptions: {
+      header: null
+    },
+    tabBarOptions: {
+      style: {
+        height: 50,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowRadius: 6,
+        shadowOpacity: 1,
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+      },
+    }
+  }
+)
+
+const MainNavigator = createAppContainer(
+  createStackNavigator({
+    Home: {
+      screen: Tabs,
+      navigationOptions: {
+        header: null
+      }
+    },
+    DeckDetails: {
+      screen: DeckDetails,
+      navigationOptions: {
+        header: null
+      }
+    }
+  })
+)
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flex: 1 }} >
+        <StatusBar backgroundColor={'#ffffff'} basStyle='dark-content' />
+        <MainNavigator />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
