b537e72cbd4ed7bed3bc1c24333d6a0eec38d8e5
DipanshKhandelwal <deepanshrocks438@gmail.com>
2019-01-23 09:48:30 +0530

    Added deck list minimal UI and deck list element

---
+++ src/components/DeckElement.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class DeckElement extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, padding: 20 }} >
+        <Text>DeckElement</Text>
+      </View>
+    )
+  }
+}
+
+export default DeckElement
--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+import DeckElement from './DeckElement'
 
 class DeckList extends React.Component {
   render() {
     return (
-      <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }} >
-        <Text>DeckList</Text>
+      <View style={{ flex:1, backgroundColor: '#cfcfcf' }} >
+        <View style={styles.headingContainer} >
+          <Text style={styles.heading} >DeckList</Text>
+        </View>
+        <View style={{ flex: 1}} >
+        <FlatList
+          style={{ backgroundColor : '#afafaf', flex: 1}}
+          data={[{key: 'a'}, {key: 'b'}, {key: 'c'}, {key: 'd'}]}
+          renderItem={({item}) => 
+            <DeckElement style={{ flex: 1 }} >{item.key}</DeckElement>
+          }
+        />
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  headingContainer: {
+    alignItems: 'center',
+    padding: 15
+  },
+  heading: {
+    fontSize: 35
+  }
+})
 
 export default DeckList
