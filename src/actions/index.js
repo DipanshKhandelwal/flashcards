9cbbdfed2306283f312f868f8fbfb65821238dab
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-01-30 11:47:58 +0530

    Added actions for adding new card

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
-import { fetchDecks, deleteDeck, saveDeck } from '../utils/api'
+import { fetchDecks, deleteDeck, saveDeck, saveCard } from '../utils/api'
 
 export const RECEIVE_DECKS = 'RECEIVE_DECKS'
 export const ADD_DECK = 'ADD_DECK'
 export const REMOVE_DECK = 'REMOVE_DECK'
+export const ADD_CARD = 'ADD_CARD'
 
 export const receiveDecks = (decks) => {
     return {
@@ -25,6 +26,14 @@ export const removeDeck = (deck) => {
   }
 }
 
+export const addCard = (card, deckId) => {
+    return {
+        type: ADD_CARD,
+        card,
+        deckId
+    }
+}
+
 export const handleReceiveDecks = () => {
     return (dispatch) => {
         return fetchDecks().then((decks) => {
@@ -45,6 +54,14 @@ export const handleRemoveDeck = (deck) => {
     return (dispatch) => {
         return deleteDeck(deck.id).then(() => {
             dispatch(removeDeck(deck))
+        })
+    }
+}
+
+export const handleAddCard = ({ question, answer, deckId }) => {
+    return (dispatch) => {
+        return saveCard(question, answer, deckId).then((card) => {
+            dispatch(addCard(card, deckId))
         })
     }
 }
